e2a90cead95541bc9a88a2ddcfc86c09551cf876
ubuntu <ubuntu@sm2028-07-mgt.storagelab.huawei.com>
2020-02-07 01:18:51 +0000

    Introduce 10-bit counter field in logical time-stamp.

--- src/dssn/Sequencer.h
+++ src/dssn/Sequencer.h
@@ -1,5 +1,4 @@
 /* Copyright (c) 2020  Futurewei Technologies, Inc.
- *
  * All rights are reserved.
  */
 #pragma once
@@ -13,22 +12,25 @@ namespace DSSN {
  * DSSN Sequencer works in a distributed model and is a lib that goes side-by-side with a Coordinator.
  * 
  * One challenge in distributed sequencer is in multi-node clock synchronization. Clock synchronization
- * s an issue ndependent from DSSN. There are many clock synchronization solutions. This implementation
+ * is a topic independent from DSSN. There are many clock synchronization solutions. This implementation
  * gets time-stamp from a PHC (Physical Hardware Clock) on a NIC/IB card which is managed by LinuxPTP.
  *
  * LinuxPTP provides sub-microsecond time synchronization precision. A sub-microsecond precision should
  * be sufficient to support 1M transactions per second.
  *
- * To avoid time-stamp collision between nodes, each Sequencer is given a 'weight' as a tie-breaker.
- * Out of the 64-bit logical time stamp, the lower 10-bit is allocated for the tie-breaker. This implies
- * a max of 1024 Sequencers can be supported. The upper 54-bit is taken directly from the PHC's micro-second 
- * readings.
+ * A logical time-stamp is a 64-bit value consisted of <44-bit phc time in usec><10-bit weight><10-bit counter> 
  *
- * [TBD: Sequencer management: How to automatically assign the 'weight' at Sequencer startup time? ]
+ * The 10-bit counter field is used by Sequencer to ensure unique logical time-stamps. This allows
+ * Sequencer to handle upto 1024 requests within a one microsecond period. More then 1024, Sequencer will
+ * need to re-read PHC until the next microsecond has arrived. 
  *
- * An embedded error detection is built-in to Sequencer by checking the same logical time-stamp is not
- * issued twice. The maximum transaction throught of a single node should be much less than 1M transaction
- * per second. So it should be impossible for this error to happen.
+ * The 10-bit weight field is used to avoid time-stamp collision between nodes. Each Sequencer is assigned
+ * a different weight and its initialization time. When multiple nodes generated logical time-stamp from the
+ * the same PHC time-stamp and the same local counter, the weight field will serve as a tie-breaker to ensure
+ * no logical time-stamp collision will happen between any node. A 10-bit weight field implies a max of 1024
+ * Sequencers can be supported. 
+ *
+ * [TBD: How to automatically assign 'weight' at Sequencer startup time? ]
  */
 class Sequencer {
     PROTECTED:
@@ -38,9 +40,10 @@ class Sequencer {
     uint64_t getTimeStamp();    // return logical time-stamp
 
     PRIVATE:
-    uint64_t readPHC();         // Read time-stamp from PHC
-
-    uint64_t    last_issued;    // Last time-stamp issued
+    uint64_t    readPHC();      // Read time-stamp from PHC
+    uint64_t    last_phc;       // Last PHC time-stamp read
+    uint32_t    counter;        // local counter to ensure unique logical time-stamp is geneerated.
+                                // If last_phc == this_phc, then counter++, else counter = 0;
 }; // end Sequencer class
 
 } // end namespace DSSN
