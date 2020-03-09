59b37e46cba68c57e9176612b6f42ea88447d23b
hfung <hfung@futurewei.com>
2020-03-09 19:46:57 +0000

    Used more high-performing scoped array

--- src/dssn/TxEntry.cc
+++ src/dssn/TxEntry.cc
@@ -16,32 +16,51 @@ TxEntry::TxEntry() {
     this->txState = TX_PENDING;
     this->commitIntentState = TX_CI_UNQUEUED;
     this->cts = 0;
+	this->writeSetSize = writeSetSize;
+	this->readSetSize = readSetSize;
 }
 
+TxEntry::TxEntry(uint32_t readSetSize, uint32_t writeSetSize) {
+	this->pi = std::numeric_limits<uint64_t>::max();
+	this->eta = 0;
+	this->txState = TX_PENDING;
+	this->commitIntentState = TX_CI_UNQUEUED;
+	this->cts = 0;
+	this->writeSetSize = writeSetSize;
+	this->readSetSize = readSetSize;
+	this->writeSet.reset(new KVLayout *[writeSetSize]);
+	this->writeSetHash.reset(new uint64_t[writeSetSize]);
+	this->writeSetInStore.reset(new KVLayout *[writeSetSize]);
+	this->readSet.reset(new KVLayout *[readSetSize]);
+	this->readSetHash.reset(new uint64_t[readSetSize]);
+	this->readSetInStore.reset(new KVLayout *[readSetSize]);
+}
+
+
 TxEntry::~TxEntry() {
-	for (uint32_t i = 0; i < writeSet.size(); i++) {
+	for (uint32_t i = 0; i < writeSetSize; i++) {
 		delete writeSet[i];
 	}
-	for (uint32_t i = 0; i < readSet.size(); i++) {
+	for (uint32_t i = 0; i < readSetSize; i++) {
 		delete readSet[i];
 	}
 }
 
 bool
-TxEntry::insertReadSet(KVLayout* kv) {
-	readSet.push_back(kv);
+TxEntry::insertReadSet(KVLayout* kv, uint32_t i) {
+	readSet[i] = kv;
 	uint64_t indexes[2];
 	RAMCloud::MurmurHash3_x64_128(kv->k.key.get(), kv->k.keyLength, 0, indexes);
-	readSetHash.push_back((indexes[0] << 32) | (indexes[1] & 0xffffffff));
+	readSetHash[i] = ((indexes[0] << 32) | (indexes[1] & 0xffffffff));
 	return true;
 }
 
 bool
-TxEntry::insertWriteSet(KVLayout* kv) {
-	writeSet.push_back(kv);
+TxEntry::insertWriteSet(KVLayout* kv, uint32_t i) {
+	writeSet[i] = kv;
 	uint64_t indexes[2];
 	RAMCloud::MurmurHash3_x64_128(kv->k.key.get(), kv->k.keyLength, 0, indexes);
-	writeSetHash.push_back((indexes[0] << 32) | (indexes[1] & 0xffffffff));
+	writeSetHash[i] = ((indexes[0] << 32) | (indexes[1] & 0xffffffff));
 	return true;
 }
 
