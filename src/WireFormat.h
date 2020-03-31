9204f7d8f56f2a8f28e8e7d80ac3f4b0de881c4c
Tony Mak <tmak@futurewei.com>
2020-03-31 18:12:46 +0000

    grouped the dssn meta data together for easier data transfer

--- src/WireFormat.h
+++ src/WireFormat.h
@@ -172,7 +172,6 @@ struct ClientLease {
 } __attribute__((packed));
 
 struct DSSNTxMeta {
-    uint64_t cts;
     uint64_t eta;
     uint64_t pi;
 } __attribute__((packed));
@@ -1216,8 +1215,7 @@ struct MultiOp {
             uint64_t version;
 
             /// SSN tx data
-            uint64_t max_eta;
-            uint64_t min_pi;
+	    DSSNTxMeta meta;
 
             /// Length of the object data following this struct.
             uint32_t length;
@@ -1342,8 +1340,7 @@ struct Read {
         ResponseCommon common;
         uint64_t version;
         /// SSN tx data
-        uint64_t max_eta;
-        uint64_t min_pi;
+        DSSNTxMeta meta;
         uint32_t length;              // Length of the object's value in bytes.
                                       // The actual bytes of the object follow
                                       // immediately after this header.
@@ -1370,8 +1367,7 @@ struct ReadKeysAndValue {
         ResponseCommon common;
         uint64_t version;
         /// SSN tx data
-        uint64_t max_eta;
-        uint64_t min_pi;
+        DSSNTxMeta meta;
         uint32_t length;              // Length of the object's keys and value
                                       // as defined in Object.h in bytes.
                                       // The actual bytes of the object follow
@@ -1867,6 +1863,7 @@ struct TxPrepare {
 
     struct Request {
         RequestCommon common;
+        uint64_t dssnCTS;
         union {
             ClientLease lease;      // Lease information for the requested
                                     // transaction.  To ensure prepare requests
