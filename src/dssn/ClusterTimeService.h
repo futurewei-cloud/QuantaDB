0261cc07966a8d3b8b3833c0f3bfaa2425c6d96e
mhsu <mhsu@futurewei.com>
2020-05-19 23:57:25 +0000

    Update DSSN Cluster Time Service comment.

--- src/dssn/ClusterTimeService.h
+++ src/dssn/ClusterTimeService.h
@@ -14,31 +14,27 @@
 namespace DSSN {
 /**
  * DSSN Cluster Time Service:
- * - High performance (~20ns delay) and high precision (nano-second) time service
+ * - High speed and high precision (nano-second) cluster time service
  * - Issues cluster unique time stamp (logical time stamp, explained below)
  * - Issues node local time stamp (real clock time stamp) in nanosecond precision.
- * - Convertion utility that converse between logical time stamp and the corresponding real time stamp.
+ * - Convertion utility converse between logical time stamp and the corresponding real time.
  *
- * DSSN Cluster Time Service relies on an underlying cluster time synchronization module to the sub-microsecond
- * precision. There are a few existing such protocols. For this implementation, we use the PTP (IEEE1588) protocol
+ * DSSN Cluster Time Service relies on an underlying time synchronization module to sync time to sub-microsecond
+ * precision. There are a few existing such protocols. In this implementation, we use the PTP (IEEE1588) protocol
  * to synchronize system clocks in the cluster.
  *
  * A logical time-stamp is a 64-bit value consisted of <54-bit real clock time in nano-sec><10-bit node id>.
- * A logical time-stamp is monotonically increasing and unique across the DSSN cluster. How these three fields
- * are generated is explained below. 
+ * A logical time-stamp is monotonically increasing and unique across the DSSN cluster. 
  *
- * The 54-bit field is real clock time in nano-seconds.
+ * The 10-bit node id field ensures logical time stamp is unique among the cluster nodes. 10 bits implies a
+ * maximum of 1024 nodes can be supported. 
  *
- * Each ClusterTS is assigned a unique id to ensures no logical time stamp collision between nodes. A 10-bit
- * id field implies a maximum of 1024 nodes can be supported. Each node runs a single Cluster Time Server instance. 
+ * Node id must be unique and is automatically assigned at ClusterTimeService class instantiation time.
+ * A formal approach to obtain a unique node id could be using a cluster management service such as zookeeper.
+ * In this implementation, a light-weight approach is used by picking up the last number of the IP address of
+ * the PTP interface port.
  *
- * Automatic node id is assigned at ClusterTimeService class instantiation.
- * A formal approach to the unique id assignment would be to hook-up to a cluster node management
- * service such as zookeeper. In this implementation, a light-weight approach is used by picking up the last
- * number of the IP address of the PTP interface port.
- *
- * High performance is achieved by getting system time from the gettimeofday() which gives micro-second precision.
- *
+ * This implementation uses PTP and gettimeofday(2) to get synchronized micro-second precision system time.
  * Sub-microsecond precision is simulated using a call counter. Monotonic increasing property is preserved.
  */
 
@@ -91,11 +87,11 @@ class ClusterTimeService {
     #define TS_TRACKER_NAME  "DSSN_TS_Tracker"
     typedef struct {
         uint64_t last_usec;             // usec from the last gettimeofday() call
-        uint32_t ctr;
+        uint32_t ctr;                   // counter
         std::atomic_flag flag;          // spin lock
     } ts_tracker_t;
 
-    ts_tracker_t * tp;                  // pointing to a shared delta tracker struct 
+    ts_tracker_t * tp;                  // pointing to a shared ts_tracker
     uint32_t node_id;		            // 
 }; // ClusterTimeService
 
