3547b41f54d042b216838548272dab007aff2cfc
Tony Mak <tmak@futurewei.com>
2020-08-07 01:16:19 +0000

    Added install feature to the cmake build.  The executables and libraries are now in the location which can be invoked by the clusterperf scripts

--- src/RAMCloudCoord.cmake
+++ src/RAMCloudCoord.cmake
@@ -13,7 +13,7 @@ add_library(ramcloudcoord SHARED
 
 link_directories(${CMAKE_CURRENT_SOURCE_DIR})
 
-list(APPEND LIBS ramcloud ramcloudcoord ramcloudserver Message)
+list(APPEND LIBS ramcloud ramcloudcoord ramcloudserver message)
 
 add_executable(coordinator CoordinatorMain.cc)
 target_link_libraries(coordinator PUBLIC ${ramcloud}
