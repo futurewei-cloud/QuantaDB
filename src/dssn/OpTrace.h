3b9988d10f827505ad75838da42992d31cc7cb47
Tony Mak <tmak@futurewei.com>
2020-09-14 17:44:40 +0000

    Added the operation tracing object.  It calculates how much time in CPU cycles a operation takes, and how many times the operation is executed as well as number of sucessful and failed operations.

---
+++ src/dssn/OpTrace.h
@@ -0,0 +1,79 @@
+/* Copyright (c) 2020 Futurewei Technologies, Inc.
+ *
+ * All rights reserved.
+ */
+
+#pragma once
+
+#include "Cycles.h"
+
+namespace DSSN {
+
+struct Metric {
+    Metric()
+      : latency(0)
+      , count(0)
+      , sCount(0)
+      , fCount(0)
+  {}
+    bool isValid() { return (count > 0); }
+    void clear() {
+        count = 0;
+	sCount = 0;
+	fCount = 0;
+	latency = 0;
+    }
+    uint64_t latency;             //Last latency value in CPU cycles
+    std::atomic<uint64_t> count;  //Cumulative count
+    std::atomic<uint64_t> sCount; //Cumulative count on succeeded operations
+    std::atomic<uint64_t> fCount; //Cumulative count on failed operations
+};
+
+/**
+ *  This class track executive time of a function or block of codes in CPU
+ *  cycles.
+ *  The cycle time is written to the buffer provided by the caller
+ */
+class OpTrace {
+ public:
+    /**
+     * Constructor of the OpTrace
+     */
+    OpTrace(Metric *metric) {
+#ifdef MONITOR
+        pMetric = metric;
+	pResult = NULL;
+        mStartTime = RAMCloud::Cycles::rdtsc();
+#endif
+ 
+    }
+
+    OpTrace(Metric *metric, bool* result) {
+#ifdef MONITOR
+        pMetric = metric;
+	pResult = result;
+        mStartTime = RAMCloud::Cycles::rdtsc();
+#endif
+    }
+
+    ~OpTrace() {
+#ifdef MONITOR
+        pMetric->latency = RAMCloud::Cycles::rdtsc() - mStartTime;
+	pMetric->count++;
+
+	if (pResult == NULL) return;
+
+	if (*pResult) {
+	    pMetric->sCount++;
+	} else {
+	    pMetric->fCount++;
+	}
+#endif
+    }
+   private:
+      uint64_t mStartTime;
+      Metric* pMetric;
+      bool* pResult;
+};
+
+}
