ba9f121a850a96620dc34943416dc4e0935f5bdd
Tony Mak <tmak@futurewei.com>
2021-04-08 13:39:40 -0700

    Evoluation 2+ eliminated the mutex from the concludequeue
        For TPCC New order only:
        - 1/1/32/32 (servers/spans/warehouses/clients), performance maintains around 190K
        - 3/3/9/9 (servers/spans/warehouses/clients), performance is increased from 17K to 24K

--- src/quantadb/ConcludeQueue.h
+++ src/quantadb/ConcludeQueue.h
@@ -32,7 +32,6 @@ class ConcludeQueue {
 
     PROTECTED:
     boost::lockfree::queue<TxEntry*> inQueue{10000};
-    std::mutex mutex;
 
     PUBLIC:
     std::atomic<uint64_t> inCount{0};
@@ -42,25 +41,19 @@ class ConcludeQueue {
 
     //for dequeueing by the consumer
     bool try_pop(TxEntry *&txEntry) {
-        mutex.lock();
         if ((inCount - outCount) > 0 && inQueue.pop(txEntry)) {
             outCount++;
-            mutex.unlock();
             return true;
         }
-        mutex.unlock();
         return false;
     }
 
     //for enqueueing by producers
     bool push(TxEntry *txEntry) {
-        mutex.lock();
         if (inQueue.push(txEntry)) {
             inCount++;
-            mutex.unlock();
             return true;
         }
-        mutex.unlock();
         return false;
     }
 }; // end ConcludeQueue class
